8bf36d1ab04684d151ba9cd381ddfe7242fdbfa7
krischal07 <krischal.shrestha9849@gmail.com>
2024-08-11 11:35:22 +0545

    Delete Task functionality added

--- to-do-app-react/src/components/Task.jsx
+++ to-do-app-react/src/components/Task.jsx
@@ -3,15 +3,21 @@ import './Task.css'
 
 
 export const Task = ({allTask, setAllTask}) => {
-    const [completedTask, setCompletedTask] = useState(false);
+    // const [completedTask, setCompletedTask] = useState(false);
 
-    const handleCheckBox = (index) =>{
-            if(completedTask.includes(index)){
-                setCompletedTask(completedTask.filter(taskIndex => taskIndex !==index));
-            }else{
-                setCompletedTask([...completedTask, index]);
-            }
+    // const handleCheckBox = (index) =>{
+    //         if(completedTask.includes(index)){
+    //             setCompletedTask(completedTask.filter(taskIndex => taskIndex !==index));
+    //         }else{
+    //             setCompletedTask([...completedTask, index]);
+    //         }
+    const handleDelete = (indexToRemove) =>{
+        let upadtedAllTask = allTask.filter((task, index) => index !== indexToRemove);
+        setAllTask(upadtedAllTask); 
+        console.log(upadtedAllTask);
+        
     }
+
   return (
     <div className='container'>
         <h2 className='head'>YOUR TASKS:</h2>
@@ -22,14 +28,15 @@ export const Task = ({allTask, setAllTask}) => {
                      <span>
                              <div className=''>
                                  <input type="checkbox" 
-                                    onChange={()=>handleCheckBox(index)}
-                                    checked={completedTask.includes(index)}
+                                    // onChange={()=>handleCheckBox(index)}
+                                    // checked={completedTask.includes(index)}
                                  />
-                               <span style={{textDecoration:completedTask.includes(index)?'line-through':'none'}}>{task}</span> 
+                               {/* <span style={{textDecoration:completedTask.includes(index)?'line-through':'none'}}>{task}</span>  */}
+                               {task}
                              </div>
                          <div className='buttons'>
                              <button><i class="fas fa-edit"></i></button>
-                             <button><i class="fas fa-trash"></i></button>
+                             <button onClick={() =>handleDelete(index)}><i class="fas fa-trash"></i></button>
                          </div>
                          </span>
                          
